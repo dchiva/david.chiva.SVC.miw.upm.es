d7adedbb467597b352c7f736be7e4ee508c5d09e
David <davidchiva3@gmail.com>
2016-10-05 19:04:10 +0200

    Añadido el test de la clase C52

---
+++ david.chiva.SVC.miw.upm.es/src/test/java/david/chiva/SVC/miw/upm/es/C52Test.java
@@ -0,0 +1,14 @@
+package david.chiva.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C52Test {
+
+	@Test
+	public void testMA() {
+		assertEquals("mA", new C52().mA());
+	}
+
+}
